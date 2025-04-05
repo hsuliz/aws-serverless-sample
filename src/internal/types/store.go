d1b85aa7db41df31426a6c311b36f8aafeda22fd
hsuliz <hlib.suliz@gmail.com>
2025-04-05 16:58:29 +0200

    feat: add method in interface

--- src/internal/types/store.go
+++ src/internal/types/store.go
@@ -6,4 +6,5 @@ type Store interface {
 	FindBooks(context.Context) (BookRange, error)
 	GetBook(context.Context, string) (*Book, error)
 	CreateBook(context.Context, Book) error
+	ModifyBook(ctx context.Context) error
 }
